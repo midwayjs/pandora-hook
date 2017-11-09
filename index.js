62d930b0dcba25fa12e7017765063520f022c9d9
Harry Chen <czy88840616@gmail.com>
2017-11-09 21:54:42 +0800

    feat: support require from outside

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
 'use strict';
-const tracer = require('./lib/trace_manager');
-const hook = require('module-hook');
-const shimmer = require('shimmer');
-const globby = require('globby');
-const path = require('path');
-const dir = path.join(__dirname, 'patch');
-
-function send(type, payload) {
-  process.emit(`pandora:hook:${type}`, payload);
-}
-
-globby.sync(['*.js', '*/**.js'], {
-  cwd: dir
-}).forEach(file => {
-  const m = require(path.join(dir, file));
-  m({
-    hook,
-    shimmer,
-    Tracer: tracer,
-    send
-  });
-});
+exports.eggLogger = require('./patch/egg-logger');
+exports.urllib = require('./patch/urllib');
