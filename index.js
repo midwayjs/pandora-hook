ecd48cbe2c77c6a9a4e6a5d7d9b9765332b340fc
Harry Chen <czy88840616@gmail.com>
2017-11-09 21:54:42 +0800

    feat: support require from outside

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
 'use strict';
-const hook = require('module-hook');
-const shimmer = require('shimmer');
-const globby = require('globby');
-const path = require('path');
-const dir = path.join(__dirname, 'patch');
-
-globby.sync(['*.js', '*/**.js'], {
-  cwd: dir
-}).forEach(file => {
-  const m = require(path.join(dir, file));
-  m(hook, shimmer);
-});
+exports.eggLogger = require('./patch/egg-logger');
+exports.urllib = require('./patch/urllib');
