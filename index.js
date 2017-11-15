689b4c40be358db41324795042c38b193daec756
Harry Chen <czy88840616@gmail.com>
2017-11-15 16:05:02 +0800

    feat: move tracer to metrics package

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 'use strict';
 exports.eggLogger = require('./patch/egg-logger');
 exports.urllib = require('./patch/urllib');
+exports.bluebird = require('./patch/bluebird');
+exports.http = require('./patch/http');
